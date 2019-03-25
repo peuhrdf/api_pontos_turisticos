a095b3b01e7c5d71c1ea1458c3a9ada461f81048
Pedro <pedrohrdf.ti@gmail.com>
2019-03-25 20:51:26 -0300

    config

--- apps/atracoes/models.py
+++ apps/atracoes/models.py
@@ -7,6 +7,7 @@ class Atracao(models.Model):
     horario_func = models.TextField()
     idade_minima = models.IntegerField()
     foto = models.ImageField(upload_to='imagens/atracoes_images', null=True, blank=True)
+    obs = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.nome
